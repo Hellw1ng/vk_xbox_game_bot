fb44f0f78c68ae55c44269f6ae85b83a685d2afa
Hellw1ng <nik.pust-97@mail.ru>
2022-08-11 13:55:44 +0300

    Bots

---
+++ TG_Xbox_game_bot.py
@@ -0,0 +1,106 @@
+import telebot
+import requests
+from bs4 import BeautifulSoup
+import json
+import test1
+from test1 import *
+import parsing
+from parsing import main
+
+
+bot = telebot.TeleBot('5382388258:AAGEep0hi5f5p63KNA7vGStHosUDkMtXomc')
+url = ("https://www.google.com/finance/quote/TRY-RUB?sa=X&ved=2ahUKEwjc4NzFl6j5AhUGjqQKHf63DIQQmY0JegQIAhAb")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет {message.from_user.first_name} {message.from_user.last_name} ,просто напишите название игры и бот подскажет вам цену '
+    bot.send_message(message.chat.id,mess)
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text.lower() == ["", " "] or len(message.text.lower()) == 1:
+        msg = message.text.lower()
+        print(msg)
+        # user_id = message.user_id
+        bot.send_message(message.chat.id, '	‼ Бот не распознает стикеры,смайлики и не используется для поиска игры по 1 букве ‼ ')
+    elif len(message.text.lower()) <= 3:
+        msg = message.text.lower()
+        print(msg)
+        # user_id = message.user_id
+        bot.send_message(message.chat.id, '‼ Используйте для поиска больше 3 символов,иначе бот подберет вам слишком большое количество игр ‼ ')
+    elif message.text.lower() == "обновить базу игр":
+        msg = message.text.lower()
+        print(msg)
+        # user_id = message.user_id
+        bot.send_message(message.chat.id, 'Начало обновления базы игр')
+        test1.main()
+        # time.sleep(20)
+        msg = message.text.lower()
+        # user_id = message.user_id
+        bot.send_message(message.chat.id, 'Конец обновления базы игр')
+    elif message.text.lower() == 'скидка' or message.text.lower() == 'скидки':
+        msg = message.text.lower()
+        print(msg)
+        # user_id = message.user_id
+        with open('games_with_sale.json', encoding="utf-8") as file:
+            games_with_sale = json.load(file)
+        n = 0
+        req = requests.get(url)
+        src = req.text
+        soup = BeautifulSoup(src, 'lxml')
+        kurs = soup.find('div', class_='YMlKec fxKbKc').text.split()
+        kurs = float(kurs[0])
+        for name, price in games_with_sale.items():
+            # for i in rep:
+            price = price.split(' ', 1)
+            smile_skidka = price[1]
+            print(smile_skidka)
+            price = float(price[0])
+            print(price)
+            price = price * kurs
+            price = round(price)
+            print(price)
+            message_price = (f"🏷 Название игры: {name}\n 💰;Цена: {price} Руб.  🔥")
+            bot.send_message(message.chat.id, message_price)
+            n = 1
+
+
+    elif message.text.lower() != ["", " "]:
+        msg = message.text.lower()
+        print(msg)
+        # user_id = message.user_id
+        with open('all_games.json', encoding="utf-8") as file:
+            all_games = json.load(file)
+        n = 0
+        req = requests.get(url)
+        src = req.text
+        soup = BeautifulSoup(src, 'lxml')
+        kurs = soup.find('div', class_='YMlKec fxKbKc').text.split()
+        kurs = float(kurs[0])
+        for name, price in all_games.items():
+            # for i in rep:
+            if msg.lower() in name.lower():
+                price = price.split(' ', 1)
+                if len(price) == 2:
+                    smile_skidka = price[1]
+                    price = float(price[0])
+                    price = price * kurs
+                    price = round(price)
+                    message_price = (f"🏷 Название игры: {name}\n 💰 Цена: {price} Руб. 🔥🔥")
+                    bot.send_message(message.chat.id, message_price)
+                    n = 1
+                else:
+                    price = float(price[0])
+                    price = price * kurs
+                    price = round(price)
+
+                    message_price = (f"🏷 Название игры: {name}\n 💰 Цена: {price} Руб.")
+                    bot.send_message(message.chat.id, message_price)
+                    n = 1
+        if n == 0:
+            message_price = '‼ Такой игры нету,проверьте правильность ввода ‼'
+            bot.send_message(message.chat.id, message_price)
+
+
+bot.polling(none_stop=True)
---
+++ parsjsonArg.py
@@ -0,0 +1,68 @@
+import json
+import re
+import time
+import requests
+from bs4 import BeautifulSoup
+from parsingArg import get_data_with_selenium
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def pars_json (lens):
+    z=1
+    n=0
+    all_games = {}
+    games_with_sale = {}
+    while z<=lens:
+        with open(f"index_selenium_arg_{z}.html",encoding="utf-8") as file:
+            src = file.read()
+        soup = BeautifulSoup(src, "lxml")
+        quotes = soup.find_all('div', class_ ='m-product-placement-item f-size-medium context-game gameDiv qlButtonFunc')
+        # print(soup.find_all('span',class_ = 'x-screen-reader').next_element)
+
+        for l,i in enumerate(quotes, start=0):
+            if i.find('span',class_='textpricenew x-hidden-focus') is not None:
+                itemName = i.find('h3', class_='c-subheading-4 x1GameName').text.strip()
+                itemPrice = i.find('span',class_='textpricenew x-hidden-focus').text.strip()
+                # itemId = i["data-bigid"]
+                # print(itemId)
+                n=n+1
+                if i.find('span',class_ = 'x-screen-reader') is not None:
+                    itemPreviousPrice = i.find('span',class_ = 'x-screen-reader').next_element.next_element
+                    print(f'{n}: Было {itemPreviousPrice} стало {itemPrice} за {itemName}',)
+                    itemPrice = itemPrice.replace(".", "")
+                    itemPrice = itemPrice.replace("₺","")
+                    itemPrice = itemPrice.replace(",", ".")
+                    itemPrice = itemPrice.replace(" ", "")
+                    itemPrice = itemPrice.replace("$", "")
+                    # itemPrice = float(itemPrice)
+                    itemPrice = itemPrice + " &#128293; "
+                    print(itemPrice)
+                    all_games[itemName] = itemPrice
+                    games_with_sale [itemName] = itemPrice
+
+                else:
+                    print(f'{n}:  {itemPrice} за {itemName}')
+                    itemPrice = itemPrice.replace(".", "")
+                    itemPrice = itemPrice.replace("₺","")
+                    itemPrice = itemPrice.replace(",", ".")
+                    itemPrice = itemPrice.replace("$", "")
+                    itemPrice = itemPrice.replace(" ", "")
+                    # itemPrice = float(itemPrice)
+                    all_games[itemName] = itemPrice
+
+        z = z + 1
+    # all_games = all_games.replace('\xa0', ' ')
+    with open ('all_games_arg.json',"w",encoding="utf-8") as file:
+        json.dump(all_games,file,indent=4,ensure_ascii=False)
+    with open ('games_with_sale_arg.json',"w",encoding="utf-8") as file:
+        json.dump(games_with_sale,file,indent=4,ensure_ascii=False)
+
+def main():
+    # get_data_with_selenium("https://www.xbox.com/tr-tr/games/all-games?xr=shellnav")
+    lens = get_data_with_selenium("https://www.xbox.com/es-AR/games/all-games?xr=shellnav")
+    pars_json(lens)
+    # pars_json(58)
+
+if __name__ == '__main__':
+    main()
